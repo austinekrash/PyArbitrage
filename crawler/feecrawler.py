ca5d27bb11dad232af2eb15e28fe72b22b81ccf5
Brotherhood <alessandro.berti94@hotmail.it>
2019-04-07 19:36:11 +0200

    crawler_to_link_to_db

--- crawler/feecrawler.py
+++ crawler/feecrawler.py
@@ -10,7 +10,7 @@ def get_record_coin(url_info, driver):
     driver.get(url_info)
     WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, ".//tbody")))
 
-    if( '()' in driver.title):
+    if('()' in driver.title):
         print("Scraping ended")
         return -1
 
@@ -19,10 +19,9 @@ def get_record_coin(url_info, driver):
     symbol = crypto.split('(')[1].split(')')[0]
 
     tbody = driver.find_elements_by_xpath('.//tbody')
-    exchanges_list = []
+    records_coin = []
     if(len(tbody) != 1):
         raise Exception("Too many tbody found!!")
-    print(symbol)
     rows = tbody[0].find_elements_by_xpath('.//tr')
     for row in rows:
         cols = row.find_elements_by_xpath('.//td')
@@ -45,19 +44,40 @@ def get_record_coin(url_info, driver):
             taker = cols[3].text.split('/')[1]
         else:
             maker = taker = cols[3].text
-        exchanges_list.append([exchange_name, min_withdrawal, withdrawal, deposit, maker, taker])
-    return [name, symbol, exchanges_list]
+        records_coin.append([symbol, exchange_name, name, float(min_withdrawal), float(withdrawal), float(deposit), float(maker), float(taker)])
+    return records_coin
 
-id_crypto = 1
-driver = webdriver.Firefox()
-record_list = []
-while True:
-    url_info = 'https://www.feexplorer.io/coin/'+str(id_crypto)+'/'
-    record = get_record_coin(url_info, driver)
-    record_list.append(record)
-    if(record is -1):
-        break
-    id_crypto = id_crypto + 1
-driver.quit()
-print(record_list)
-#Exchange, Min-with, with, (espresso in coin corrente) deposit, maker, taker (ultimi due in percentuale)
+def insert_in_db(records_all):
+    try:
+        conn = psycopg2.connect("dbname='arbitraggio' user='ale' host='localhost' password='pippo'")
+    except:
+        print("I am unable to connect to the database")
+    cur = conn.cursor()
+    sql = """INSERT INTO bitfinex(symbol, base_asset, quote_asset, symbol_std) VALUES(%s, %s, %s, %s) ON CONFLICT (symbol) DO NOTHING;"""
+    cur.executemany(sql, records_all)
+    conn.commit()
+    count = cur.rowcount
+    print (count, "Record inserted successfully into BITFINEX table")
+    if(count != len(records_all)):
+        print("#n of items inserted differs from those received: "+str(count)+" - "+str(len(records_all)))
+    cur.close()
+    conn.close()
+
+def main():
+    id_crypto = 1
+    driver = webdriver.Firefox()
+    records_all = []
+    while id_crypto < 3:
+        print(id_crypto)
+        url_info = 'https://www.feexplorer.io/coin/'+str(id_crypto)+'/'
+        records_coin = get_record_coin(url_info, driver)
+        if(records_coin is -1):
+            break
+        records_all.extend(records_coin)
+        id_crypto = id_crypto + 1
+    driver.quit()
+    #insert_in_db(records_all)
+    print(records_all)
+    #Exchange, Min-with, with, (espresso in coin corrente) deposit, maker, taker (ultimi due in percentuale)
+
+main()
