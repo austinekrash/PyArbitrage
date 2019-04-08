f06db6efa11e3cd596c4f7899dc6b954f6a50f81
eug <eugypalu@gmail.com>
2019-04-08 20:01:29 +0200

    bla

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -34,7 +34,7 @@ except:
 
 cur = conn.cursor()
 
-cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'VIEW'")
+cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'VIEW' AND table_name = 'intersection_binance_poloniex'")
 records = cur.fetchall()
 
 for x in records:
