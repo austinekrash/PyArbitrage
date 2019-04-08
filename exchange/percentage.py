4c51594208b0f4852e97c7c98c3af2fedfef4c74
eug <eugypalu@gmail.com>
2019-04-08 18:06:15 +0200

    bla

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -8,13 +8,13 @@ import cex
 import poloniex
 
 def percentage(cryptoIntersection):
-    exchange1 = cryptoIntersection[3]
-    exchange2 = cryptoIntersection[4]
+    exchange1 = (cryptoIntersection[3])[0].upper() + (cryptoIntersection[3])[1:]
+    exchange2 = (cryptoIntersection[4])[0].upper() + (cryptoIntersection[4])[1:]
     symbol1 = cryptoIntersection[0]
     symbol2 = cryptoIntersection[1]
     baseAsset = cryptoIntersection[2]
-    price1 = eval((cryptoIntersection[3])[0].upper()).Factory().get_price_pairs(cryptoIntersection[0])
-    price2 = eval((cryptoIntersection[4])[0].upper()).Factory().get_price_pairs(cryptoIntersection[1])
+    price1 = eval(exchange1).Factory().get_price_pairs(symbol1)
+    price2 = eval(exchange2).Factory().get_price_pairs(symbol2)
     if(price1 >= price2):
         perc = (price1 - price2) / price2 * 100
         return {"percentage": perc ,"startExchange": exchange2, "startSymbol": symbol2, "startPrice": price2, "endExchange": exchange1, "endSymbol": symbol1, "endPrice": price1}
