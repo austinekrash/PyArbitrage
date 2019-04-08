42b109635ee6ae6cf4c8acdfbadfbc1cdd6cad4d
eug <eugypalu@gmail.com>
2019-04-08 17:23:20 +0200

    bla

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -1,6 +1,11 @@
 import psycopg2
 import requests
 import json
+import exchange.binance
+import exchange.bitfinex
+import exchange.bittrex
+import exchange,cex
+import exchange.poloniex
 
 intersectionView = []
 perc = []
@@ -18,8 +23,6 @@ records = cur.fetchall()
 for x in records:
     intersectionView.append(x[0]) #faccio una lista con tutte le view contenenti le intersezioni tra exchange
 
-print(intersectionView)
-
 #per ogni exchange prendo la lista delle crypto
 for view in intersectionView:
     cur.execute("SELECT * FROM "+view)
@@ -28,18 +31,19 @@ for view in intersectionView:
     for x in cur.fetchall():#prendo tutte le tuple per ogni view
         cryptoIntersection.append(x)
         for i in cryptoIntersection:
-            pass
+            price1 = cryptoIntersection[i][3]().get_price_pairs(cryptoIntersection[i][0])
+            price2 = cryptoIntersection[i][4]().get_price_pairs(cryptoIntersection[i][1])
+            percentage = percentage(price1, price2)
+            print(percentage)
+            
             #chiamo api prezzo su symbol
             #inserisco in perc la coppia o tripla symbol std_symbol percentuale
             #non ha senso prendere anche il prezzo, perchè il prezzo va preso subito prima della vednita/Acquisto
 
-"""
-def percentage(crypto1, crypto2):
-    price1# = price della tupla che gli viene passata
-    price2# = price della tupla che gli viene passata
-    if(price1 >= price 2):
+
+def percentage(price1, pryce2):
+    if(price1 >= price2):
         perc = (price1 - price2) / price2 * 100
     else:
         perc = (price2 - price1) / price1 * 100
     #return tupla con percentuale, exchange di partenza, exchange di destinazione
-"""
