077c3405dbd021d3a11b24833a9be50a1ea44bfa
eug <eugypalu@gmail.com>
2019-04-10 19:31:59 +0200

    robe

--- exchange/main.py
+++ exchange/main.py
@@ -8,8 +8,36 @@ from poloni import Poloniex
 BINANCEapiKey = 'WXYox2eh9V8fUiLvjdW9f8xkh3q30EpzxGaeLQvxMZ0TTUyDSaIJliEmAXr2NtYN'
 BINANCEsecretKey = 'qbXdJU82w1QKpjHe6OFRMdJMxbZgQuHigLRzssDqf3TCpEaxyAaHyrGolDQzbrbD'
 
+def is_advantages(startAmount, endAmount):
+    if startAmount >= endAmount:
+        return False
+    else:
+        return True
 
-def arbitrage_fee(startExchange, endExchange, symbolStart, symbolEnd,):
+
+def arbitrage_fee(startExchange, endExchange, symbolStart, symbolEnd, priceStart, priceEnd, startAmount):
+    try:
+        conn = psycopg2.connect("dbname='arbitraggio' user='ale' host='localhost' password='pippo'")
+    except:
+        print("I am unable to connect to the database")
+    #SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = 'BTC' AND exchange = 'binance'
+    cur = conn.cursor()
+
+    cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = " + symbolStart +  "AND exchange =" + startExchange)
+    start = cur.fetchall()
+
+    cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = " + symbolEnd +  "AND exchange =" + endExchange)
+    end = cur.fetchall()
+    print(start)
+    withdrawalFee = 0 #query
+    depositFee = 0 #query
+    taker = 0 #query
+    startWithdrawal = startAmount - withdrawalFee
+    endWithdrawal = startWithdrawal - depositFee
+    sellCurr = endWithdrawal - endWithdrawal * taker
+
+
+arbitrage_fee('bitfinex', 'binance', 'BTC', 'BTC', '1', '1', '10', conn)
 
 
 POLONIEXapiKey = '8QF9DS6A-YJWQQLWW-ZKUM8YV8-YJ5HG70E'
