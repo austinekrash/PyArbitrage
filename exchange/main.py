1e9ed3ccb340f559a307401c01b7f19c7e6867b2
Brotherhood <alessandro.berti94@hotmail.it>
2019-04-10 11:31:45 +0200

    some fix binance

--- exchange/main.py
+++ exchange/main.py
@@ -12,32 +12,28 @@ BINANCEsecretKey = 'qbXdJU82w1QKpjHe6OFRMdJMxbZgQuHigLRzssDqf3TCpEaxyAaHyrGolDQz
 cex = Cex.Factory()
 cex.sync()
 cex.get_price_pairs('ETH/BTC')
-'''
+
 binance = Binance.Factory(BINANCEapiKey, BINANCEsecretKey)
 binance.sync()
 binance.get_deposit_address('PAX')
 binance.get_balance('XRP')
 binance.get_balances()
 print(binance.is_frozen('BTC'))
-
+'''
 '''
 bitfinex = Bitfinex.Factory()
 bitfinex.sync()
 bitfinex.get_price_pairs('ETHBTC')
 '''
-''''
+
 bittrex = Bittrex.Factory()
 bittrex.sync()
 #bittrex.get_price_pairs('BTC-GRIN')
-#bittrex.get_deposit_address('BTC')
-bittrex.get_balances()
+bittrex.get_deposit_address('BTC')
+#bittrex.get_balances()
 #bittrex.get_open_orders('BTC-GRIN')
-"""
-bittrex = Bittrex.Factory()
-bittrex.sync()
-bittrex.get_price_pairs('BTC-GRIN')
-bittrex.getDepositAddress('test')
-"""
+
+'''
 bitfinex = Bitfinex().Factory()
 bitfinex.get_address('BTC')
 
