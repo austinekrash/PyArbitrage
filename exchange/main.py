615940a931933b97e9cffd2edd01f193fb6c3867
eug <eugypalu@gmail.com>
2019-04-10 11:36:48 +0200

    cancellati file inutili

--- exchange/main.py
+++ exchange/main.py
@@ -4,6 +4,18 @@ from bitfinex import Bitfinex
 from bittrex import Bittrex
 from poloni import Poloniex
 
+
+
+
+
+
+
+
+
+
+
+
+'''
 p = Poloniex.Factory()
 print(p.return_fee_info())
 #print(p.return_deposit_address())
@@ -11,15 +23,10 @@ print(p.return_fee_info())
 
 
 
-
-BINANCEapiKey = 'WXYox2eh9V8fUiLvjdW9f8xkh3q30EpzxGaeLQvxMZ0TTUyDSaIJliEmAXr2NtYN'
-BINANCEsecretKey = 'qbXdJU82w1QKpjHe6OFRMdJMxbZgQuHigLRzssDqf3TCpEaxyAaHyrGolDQzbrbD'
-
-'''
 cex = Cex.Factory()
 cex.sync()
 cex.get_price_pairs('ETH/BTC')
-'''
+
 binance = Binance.Factory(BINANCEapiKey, BINANCEsecretKey)
 binance.sync()
 binance.get_deposit_address('PAX')
@@ -27,19 +34,18 @@ binance.get_balance('XRP')
 binance.get_balances()
 print(binance.is_frozen('BTC'))
 
-'''
+
 bitfinex = Bitfinex.Factory()
 bitfinex.sync()
 bitfinex.get_price_pairs('ETHBTC')
-'''
-''''
+
 bittrex = Bittrex.Factory()
 bittrex.sync()
 #bittrex.get_price_pairs('BTC-GRIN')
 #bittrex.get_deposit_address('BTC')
 bittrex.get_balances()
 #bittrex.get_open_orders('BTC-GRIN')
-"""
+
 bittrex = Bittrex.Factory()
 bittrex.sync()
 bittrex.get_price_pairs('BTC-GRIN')
@@ -48,14 +54,6 @@ bittrex.getDepositAddress('test')
 bitfinex = Bitfinex().Factory()
 bitfinex.get_address('BTC')
 
-<<<<<<< HEAD
-poloniex = Poloniex().Factory()
-poloniex.get_deposit_address('BTC')
-"""
-'''
-=======
-
->>>>>>> e53adde140886e0833c34ce1f1e0ee9a4df98f12
 poloniex = Poloniex.Factory()
 poloniex.sync()
 poloniex.get_price_pairs('BTC_BCN')
