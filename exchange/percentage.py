1bd96c3c5b4ca82964216c5eb5b0485d5050220b
Brotherhood <alessandro.berti94@hotmail.it>
2019-04-11 20:14:28 +0200

    feeing

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -114,7 +114,7 @@ def is_advantages(startAmount, endAmount):
 def arbitrage_fee(startExchange, endExchange, pairStart, pairEnd, priceStart, priceEnd, setAmount, percentage, conn, cur):
     symbolStart = eval(startExchange).find_asset(pairStart)
     print(symbolStart)
-    symbolEnd = eval(endExchange).find_asset(pairEnd)[0]
+    symbolEnd = eval(endExchange).find_asset(pairEnd)
     print(symbolEnd)
     cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee WHERE symbol = '" + symbolStart +  "' AND exchange ='" + startExchange + "'")
     start = cur.fetchall()
