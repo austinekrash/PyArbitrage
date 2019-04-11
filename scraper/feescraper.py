1065bb406c8bced8a6fdc86598f92fac7105e3fe
Brotherhood <alessandro.berti94@hotmail.it>
2019-04-11 23:40:24 +0200

    scraper

--- scraper/feescraper.py
+++ scraper/feescraper.py
@@ -72,7 +72,7 @@ def close_db(conn, cur):
 
 
 def main():
-    id_crypto = 905
+    id_crypto = 1087
     options = Options()  #Needed since no display
     options.headless = True
     conn, cur = connect_db()
