30f55947954f210e1f28144b626195e71b50a1e0
eug <eugypalu@gmail.com>
2019-04-11 12:08:07 +0200

    calcolo fee

--- exchange/main.py
+++ exchange/main.py
@@ -43,7 +43,7 @@ def arbitrage_fee(startExchange, endExchange, pairStart, pairEnd, priceStart, pr
     cur = conn.cursor()
     cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = '" + symbolStart +  "' AND exchange ='" + startExchange + "'")
     start = cur.fetchall()
-    cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = " + symbolEnd +  "AND exchange =" + endExchange +"'")
+    cur.execute("SELECT min_widthdrawal, withdrawal, deposit, maker, taker FROM fee  WHERE symbol = '" + symbolEnd +  "' AND exchange ='" + endExchange +"'")
     end = cur.fetchall()
     print(start)
     withdrawalFee = start[0][1] #query
