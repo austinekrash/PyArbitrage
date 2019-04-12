caecc5d15cd3526c9188b2066ee5778f166daa22
eug <eugypalu@gmail.com>
2019-04-12 18:29:34 +0200

    robe su base e quote

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -114,11 +114,11 @@ def is_advantages(startAmount, endAmount):
         return True
 
 def arbitrage_fee(startExchange, endExchange, pairStart, pairEnd, priceStart, priceEnd, setAmount, percentage, conn, cur):
+    print(pairStart)
+    print(pairEnd)
     symbolStart = eval(startExchange).find_asset(pairStart)[0]
     symbolEnd = eval(endExchange).find_asset(pairEnd)[0]
     if symbolStart != symbolEnd:
-        print(pairStart)
-        print(pairEnd)
         print(symbolStart)
         print(symbolEnd)
         print("NOT EQUALS")
