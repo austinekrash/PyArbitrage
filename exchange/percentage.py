74704d2c60e8312298b0d94ec130ee72d0c94da4
eug <eugypalu@gmail.com>
2019-04-14 16:12:59 +0200

    robe

--- exchange/percentage.py
+++ exchange/percentage.py
@@ -263,6 +263,7 @@ conn, cur = open_db()
 intersectionView = fetch_views_db(cur)
 percentages = compute_percentages(intersectionView, cur)
 #orderded_nop_percentages = remove_sort_duplicates(percentages)
+print(type(percentages[0]['percentage']))
 orderded_nop_percentages = sorted(percentages, key=lambda k: k['percentage'], reverse=True) 
 
 
